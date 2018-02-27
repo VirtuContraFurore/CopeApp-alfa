d78b0fae9dfc98da928818febcf58a896cb4e1c5
Fabio Tessaro <fabio.tessaro.porta@gmail.com>
2018-02-27 16:37:03 +0100

    nuovi sfondi login

--- WebApp/WebContent/index.js
+++ WebApp/WebContent/index.js
@@ -1,7 +1,13 @@
 app.controller("IndexCtrl", IndexCtrl);
-function IndexCtrl($scope,$state){;
+
+function IndexCtrl($scope,$state){
+	
 	$scope.goto= function(state){
 		$state.go(state);
 	}
+	
 	$scope.loggedin=false;
+	
+	$scope.screenSize = "1920x1080";
+	$scope.backgroundTag = "landscape";
 }
--- WebApp/WebContent/login/login.js
+++ WebApp/WebContent/login/login.js
@@ -1,20 +1,11 @@
-app.config(function($stateProvider){
-	$stateProvider.state("login", {
-		url:"/",
-		templateUrl:"login/login.html"
-	})
-});
 app.controller("LoginCtrl", LoginCtrl);
+
 function LoginCtrl($scope, $state){
+	
 	$scope.user={
 			name: '',
 			password: ''	
 	}
-	var pattern = Trianglify({
-		width: window.innerWidth+10,
-		height: window.innerHeight+10,
-		variance: 1
-	});
-	var a= pattern.png();
-	$('#loginBackground').css('background-image', 'url(' + a + ')');
+
+	$('#loginBackground').css('background-image', 'url(http://source.unsplash.com/'+$scope.screenSize+'/?'+$scope.backgroundTag+')');
 }
