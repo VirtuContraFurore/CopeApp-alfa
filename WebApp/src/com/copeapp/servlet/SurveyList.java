44ee1113f8376ecb4485b71d05834c93b135fc10
rickycraft <ricky.roveri@gmail.com>
2018-04-22 13:48:19 +0200

    aggiunta surveyCreate servlet e aggiunti javadoc delle librerie più importanti

--- WebApp/src/com/copeapp/servlet/SurveyList.java
+++ WebApp/src/com/copeapp/servlet/SurveyList.java
@@ -1,23 +1,18 @@
 package com.copeapp.servlet;
 
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.Query;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.beanutils.BeanUtils;
-
-import com.copeapp.dto.commons.AnswerDTO;
 import com.copeapp.dto.commons.GenericErrorDTO;
-import com.copeapp.dto.commons.RoleDTO;
-import com.copeapp.entities.Role;
 import com.copeapp.entities.Survey;
 import com.copeapp.survey.SurveyRequestListDTO;
 import com.copeapp.survey.SurveyResponseListDTO;
@@ -25,6 +20,7 @@ import com.copeapp.survey.SurveyResponseMiniDTO;
 import com.copeapp.tomcat9Misc.StartupOperations;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+@WebServlet("/rest/surveylist")
 public class SurveyList extends HttpServlet{
 
 	private static final long serialVersionUID = 1L;
@@ -32,13 +28,12 @@ public class SurveyList extends HttpServlet{
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		ObjectMapper om = new ObjectMapper();
-		SurveyRequestListDTO loginRequest = om.readValue(request.getInputStream(), SurveyRequestListDTO.class);		
-		SurveyResponseListDTO loginResponse = new SurveyResponseListDTO();
+		SurveyRequestListDTO surveyListRequest = om.readValue(request.getInputStream(), SurveyRequestListDTO.class);		
 		
 		EntityManager entitymanager = StartupOperations.emfactory.createEntityManager();
 		entitymanager.getTransaction().begin(); //dato che è una select la transaction è inutile
 		Query query;
-		if	(loginRequest.isTipo()) {
+		if	(surveyListRequest.isTipo()) {
 			query = entitymanager.createQuery("SELECT s FROM surveys u WHERE (s.closeSurveyDate > current_date) order by date(s.closeSurveyDate) desc ", Survey.class);
 		} else {
 			query = entitymanager.createQuery("SELECT s FROM surveys u WHERE (s.closeSurveyDate < current_date) order by date(s.closeSurveyDate) desc ", Survey.class);
@@ -53,8 +48,8 @@ public class SurveyList extends HttpServlet{
 			SurveyResponseMiniDTO tmp = new SurveyResponseMiniDTO(s.getSurveyId(), s.getQuestion(), s.getCloseSurveyDate(), 10);
 			miniDTO.add(tmp);
 		}
-		loginResponse.setSurveyMini(miniDTO);
-		om.writeValue(response.getOutputStream(), loginResponse);
+		
+		om.writeValue(response.getOutputStream(), new SurveyResponseListDTO(miniDTO));
 		} catch (NoResultException nre) {
 			GenericErrorDTO errorResponse = new GenericErrorDTO(nre.getStackTrace(), 401, "Utente non trovato");
 			response.setStatus(401);
--- WebApp/src/com/copeapp/survey/SurveyResponseListDTO.java
+++ WebApp/src/com/copeapp/survey/SurveyResponseListDTO.java
@@ -2,10 +2,14 @@ package com.copeapp.survey;
 
 import java.util.ArrayList;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NonNull;
 
+@AllArgsConstructor
 @Data
 public class SurveyResponseListDTO {
-
+	
+	@NonNull
 	private ArrayList<SurveyResponseMiniDTO> surveyMini= new ArrayList<SurveyResponseMiniDTO>();
 }
