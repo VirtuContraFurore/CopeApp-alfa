54a10de65e1d1ef50b32edd48e4bdfa512b4d187
rickycraft <ricky.roveri@gmail.com>
2018-05-01 20:43:33 +0200

    validationUtility cambiato da void a boolean

--- WebApp/src/com/copeapp/utilities/ObjectsValidationUtility.java
+++ WebApp/src/com/copeapp/utilities/ObjectsValidationUtility.java
@@ -8,21 +8,24 @@ import lombok.NonNull;
 
 public interface ObjectsValidationUtility {
 
-	public static void validateNotNullParameters (Object object) throws LoginFailedException {
-		
+	public static boolean validateNotNullParameters (Object object) throws LoginFailedException {
+		boolean b = true;
 		Field[] declaredFields = object.getClass().getDeclaredFields();
 		for(Field field : declaredFields) {
 			field.setAccessible(true);
 			if(field.isAnnotationPresent(NonNull.class)) {
 				try {
 					if (field.get(object) == null) {
+						b= false;
 						throw new LoginFailedException(HttpStatusUtility.BAD_REQUEST, "Alcuni dei parametri obbligatori sono vuoti");
 					}
 				} catch (IllegalArgumentException | IllegalAccessException e) {
+					b= false;
 					e.printStackTrace();
 					throw new LoginFailedException(HttpStatusUtility.INTERNAL_SERVER_ERROR, "Errore interno al server", e);
 				}
 			}
 		}
+		return b;
 	}
 }
